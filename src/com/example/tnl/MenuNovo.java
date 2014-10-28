5f5d691ad8b6a146a1bae27d8c5dc9c6b58924da
alexandrebenini <alexandrebeninitj@hotmail.com>
2014-10-28 20:43:36 -0200

    novo

--- src/com/example/tnl/MenuNovo.java
+++ src/com/example/tnl/MenuNovo.java
@@ -1,15 +1,44 @@
 package com.example.tnl;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuNovo extends Activity {
-
+	Button btcadastrousuario,btcadastrolavoura;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu_novo);
+		
+		btcadastrolavoura = (Button) findViewById(R.id.btcadastrolavoura);
+		btcadastrolavoura.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// aqui 3 parte
+				Intent TrocaTela = new Intent(MenuNovo.this,CadastroLavoura.class);
+				MenuNovo.this.startActivity(TrocaTela);
+				MenuNovo.this.finish();
+			}
+		});
+		// aqui 2 parte
+		        
+				btcadastrousuario = (Button) findViewById(R.id.btcadastrousuario);
+				btcadastrousuario.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View arg0) {
+						// aqui 3 parte
+						Intent TrocaTela1 = new Intent(MenuNovo.this,CadastroUsuario.class);
+						MenuNovo.this.startActivity(TrocaTela1);
+						MenuNovo.this.finish();
+					}
+				});
 	}
 
 	@Override
