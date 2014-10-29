9e6ee2dba58d1213ff2e0195a27416688f4cd236
PauloJoseCagol <paulo-jose-cagol@hotmail.com>
2014-10-29 15:43:52 -0200

    Inserindo tecnico no Banco!!

--- src/com/example/tnl/CadastroTecnico.java
+++ src/com/example/tnl/CadastroTecnico.java
@@ -1,18 +1,30 @@
 package com.example.tnl;
 
+import com.example.tnl.models.TecnicoDAO;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroTecnico extends Activity {
+	TecnicoDAO tecnicoDAO = new TecnicoDAO(this);
 	Button btSalvarTecnico;
+	
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cadastro_tecnico);
+		
+		
+		
 		
 		// aqui 2 parte
 		btSalvarTecnico = (Button) findViewById(R.id.btSalvarTecnico);
@@ -21,9 +33,21 @@ public class CadastroTecnico extends Activity {
 					@Override
 					public void onClick(View arg0) {
 						// aqui 3 parte
-						Intent TrocaTela = new Intent(CadastroTecnico.this,Principal.class);
-						CadastroTecnico.this.startActivity(TrocaTela);
-						CadastroTecnico.this.finish();
+						
+						EditText nome = (EditText)findViewById(R.id.txNomeTecnico);
+						
+							tecnicoDAO.open();
+							tecnicoDAO.insere(nome.getText().toString());
+							tecnicoDAO.close();
+							//Voltando para tela de login
+							Intent TrocaTela = new Intent(CadastroTecnico.this,Principal.class);
+							CadastroTecnico.this.startActivity(TrocaTela);
+							CadastroTecnico.this.finish();
+						
+							
+						
+						
+						
 					}
 				});
 	}
